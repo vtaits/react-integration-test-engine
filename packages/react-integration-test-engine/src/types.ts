460e7a374aff5858608a593c880a7ab38b177957
Vadim Taits <taizvadim@gmail.com>
2023-10-13 17:54:11 +0300

    WIP: docs

--- packages/react-integration-test-engine/src/types.ts
+++ packages/react-integration-test-engine/src/types.ts
@@ -1,6 +1,11 @@
 import type { EventType, RenderResult } from "@testing-library/react";
 import type { ReactElement } from "react";
 
+/**
+ * Type of `testing-library` query
+ *
+ * See https://testing-library.com/docs/queries/about/#priority
+ */
 export enum AccessorQueryType {
 	Role = "Role",
 	LabelText = "LabelText",
@@ -12,12 +17,44 @@ export enum AccessorQueryType {
 	TestId = "TestId",
 }
 
+/**
+ * Type of the result of `testing-library` query
+ *
+ * See https://testing-library.com/docs/queries/about/#types-of-queries
+ */
 export type AccessorsType = Readonly<{
+	/**
+	 * @returns all matched elements
+	 * @throws if there are no matched elements
+	 */
 	getAll: () => HTMLElement[];
+	/**
+	 * @returns a single matched element
+	 * @throws if there are no matched elements
+	 * @throws if there are more than one matched elements
+	 */
 	get: () => HTMLElement;
+	/**
+	 * @returns all matched elements
+	 */
 	queryAll: () => HTMLElement[];
+	/**
+	 * @returns a single matched element or null
+	 * @throws if there are more than one matched elements
+	 */
 	query: () => HTMLElement | null;
+	/**
+	 * wait for mathed elements and return them
+	 * @returns promise with all matched elements
+	 * @throws if there are no matched elements
+	 */
 	findAll: () => Promise<HTMLElement[]>;
+	/**
+	 * wait for single mathed element and return them
+	 * @returns promise with all matched elements
+	 * @throws if there are no matched elements
+	 * @throws if there are more than one matched elements
+	 */
 	find: () => Promise<HTMLElement>;
 }>;
 
@@ -56,6 +93,18 @@ export type AccessorParamsType = Readonly<
 	  }
 >;
 
+/**
+ * A wrapper for current component. Can be used for integration
+ * with `react-router`, `react-redux` or other component that provided context
+ * that used in target component
+ *
+ * See an example https://github.com/vtaits/react-integration-test-engine/blob/main/packages/react-integration-test-engine/src/tests/wrappers.test.tsx
+ *
+ * @param children react tree that should be wrapped
+ * @param params parameters of the wrapper
+ * @returns a tupple with first element is the wrapped react tree and
+ * the second element is the result of the wrapper that can be accessed from engine
+ */
 export type WrapperType<
 	ParamsType extends Record<string, unknown>,
 	ResultType,
@@ -73,11 +122,57 @@ export type OptionsType<
 	>,
 	FireEvents extends Record<string, [keyof Queries, EventType]>,
 > = Readonly<{
+	/**
+	 * An object whose values is queries to rendered elements, and keys can be used to access them
+	 *
+	 * ```
+	 * const render = create(Component, defaultProps, {
+	 *   queries: {
+	 *     listItem: {
+	 *       query: AccessorQueryType.LabelText,
+	 *       parameters: ["the item of the list"],
+	 *     },
+	 *
+	 *     navigation: {
+	 *       query: AccessorQueryType.Role,
+	 *       parameters: ["navigation"],
+	 *     },
+	 *   },
+	 * });
+	 *
+	 * const engine = render({});
+	 *
+	 * const listItems = page.accessors.listItem.getAll();
+	 * const navigation = page.accessors.navigation.get();
+	 * ```
+	 */
 	queries: Queries;
 	wrappers?: Wrappers;
 	wrapperDefaultParams?: {
 		[Key in keyof Wrappers]: Parameters<Wrappers[Key]>[1];
 	};
+	/**
+	 * An object whose values is tupples of keys of queries and names of events
+	 *
+	 * ```
+	 * const render = create(Component, defaultProps, {
+	 *   queries: {
+	 *     button: {
+	 *       query: AccessorQueryType.Text,
+	 *       parameters: ["Click me"],
+	 *     },
+	 *   },
+	 *
+	 *   fireEvents: {
+	 *     buttonClick: ["button", "click"],
+	 *   },
+	 * });
+	 *
+	 * const engine = render({});
+	 *
+	 * engine.fireEvent("buttonClick");
+	 * ```
+	 */
 	fireEvents?: FireEvents;
 }>;
 
@@ -90,13 +185,65 @@ export type EngineType<
 	>,
 	FireEvents extends Record<string, [keyof Queries, EventType]>,
 > = Readonly<{
+	/**
+	 * Result of `render` of `@testing-library/react`
+	 */
 	qs: RenderResult;
+	/**
+	 * An object whose keys are keys of the `queries` parameter and whose values are accessors (see `AccessorsType`)
+	 *
+	 * ```
+	 * const render = create(Component, defaultProps, {
+	 *   queries: {
+	 *     listItem: {
+	 *       query: AccessorQueryType.LabelText,
+	 *       parameters: ["the item of the list"],
+	 *     },
+	 *
+	 *     navigation: {
+	 *       query: AccessorQueryType.Role,
+	 *       parameters: ["navigation"],
+	 *     },
+	 *   },
+	 * });
+	 *
+	 * const engine = render({});
+	 *
+	 * const listItems = page.accessors.listItem.getAll();
+	 * const navigation = page.accessors.navigation.get();
+	 * ```
+	 */
 	accessors: {
 		[Key in keyof Queries]: AccessorsType;
 	};
 	wrappers: {
 		[Key in keyof Wrappers]: ReturnType<Wrappers[Key]>[1];
 	};
+	/**
+	 * Fire an event in `act` wrapper
+	 * @param eventKey a key of the `fireEvents` parameter
+	 * @param options optional, an event object
+	 * @throws if `fireEvents` is not provided
+	 *
+	 * ```
+	 * const render = create(Component, defaultProps, {
+	 *   queries: {
+	 *     button: {
+	 *       query: AccessorQueryType.Text,
+	 *       parameters: ["Click me"],
+	 *     },
+	 *   },
+	 *
+	 *   fireEvents: {
+	 *     buttonClick: ["button", "click"],
+	 *   },
+	 * });
+	 *
+	 * const engine = render({});
+	 *
+	 * engine.fireEvent("buttonClick");
+	 * ```
+	 */
 	// biome-ignore lint/complexity/noBannedTypes: the format of `@testing-library/react`
 	fireEvent: (eventKey: keyof FireEvents, options?: {} | undefined) => void;
 }>;
